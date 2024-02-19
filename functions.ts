58760d0f5cae78d64ec691e382d826554d7fd8d4
Gabriel Ramos <gabrielrammus42@gmail.com>
2024-02-19 15:41:32 -0300

    adjusting IF

--- functions.ts
+++ functions.ts
@@ -8,10 +8,10 @@ function saudacao(nome:string) {
     const bv = "Olá " + nome;
 
     if(nome === "Gabriel" ){
-        console.log(`${bv} seja bem vindo.`);
+        return `${bv} seja bem vindo.`;
     } else {
-        console.log(`Nome não corresponde ao cadastro`);
+        return `Nome não corresponde ao cadastro`;
     }
 }
 
-saudacao("Paulo");
+console.log(saudacao("Gabriel"));
